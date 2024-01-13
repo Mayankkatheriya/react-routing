48f9ecaa64c4b63ee87f67e9571e112e0b13cd1e
Mayankkatheriya <mayankkatheriya4@gmail.com>
2024-01-13 17:10:08 +0530

    responsive navbar created

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,23 +4,25 @@ import { useState } from "react";
 import Lists from "./Lists";
 import { Link } from "react-router-dom";
 function Header() {
-  const [currentPage, setCurrentPage] = useState(()=>{
-    let page = localStorage.getItem("currrentPage")
-    return (page) ? page : "Home"
-  })
+  const [currentPage, setCurrentPage] = useState(() => {
+    let page = localStorage.getItem("currrentPage");
+    return page ? page : "Home";
+  });
   const listItem = [
     { title: "Home", path: "/" },
     { title: "Quote", path: "/quotes" },
     { title: "Resturants", path: "/restaurants" },
     { title: "Foods", path: "/foods" },
-    { title: "Contact", path: "/contact" }
-  ]
+    { title: "Contact", path: "/contact" },
+  ];
+  const [showHamburger, setHamburger] = useState(false);
 
   const handlePage = (page) => {
-    setCurrentPage(page)
+    setCurrentPage(page);
+    setHamburger(false)
     //set in localStorage
-    localStorage.setItem('currrentPage', page);
-  }
+    localStorage.setItem("currrentPage", page);
+  };
   return (
     <header className="header">
       <div className="nav">
@@ -28,10 +30,22 @@ function Header() {
           <img src={logo} alt="Logo" />
           <h1>GeeksFoods</h1>
         </Link>
-        <Lists listItem = {listItem} func = {handlePage} currentPage={currentPage}/>
+        <div className={showHamburger ? "mobile-nav-menu" :"nav-menu"}>
+          <Lists
+            listItem={listItem}
+            func={handlePage}
+            currentPage={currentPage}
+            className={"nav-menu"}
+          />
+        </div>
         <div className="nav-btn-container">
           <button className="nav-btn">Get Started</button>
-          <i className="bx bx-menu"></i>
+        </div>
+        <div className="hamburger">
+          <i
+            onClick={() => setHamburger(!showHamburger)}
+            className="fa-solid fa-bars-staggered"
+          ></i>
         </div>
       </div>
     </header>
--- src/Components/Header/Lists.js
+++ src/Components/Header/Lists.js
@@ -1,28 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {nanoid} from 'nanoid'
+import { nanoid } from "nanoid";
 
-const Lists = ( {listItem, func, currentPage}) => {
+const Lists = ({ listItem, func, currentPage }) => {
   return (
-    <div className="nav-menu">
-      <ul>
-        {listItem.map((item) => {
-          return (
-            <li key={nanoid()} onClick={() => func(item.title)}>
-              <Link
-                to={item.path}
-                style={{
-                  color:
-                    item.title === currentPage ? "rgb(59, 130, 246)" : "#fff",
-                }}
-              >
-                {item.title}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <ul>
+      {listItem.map((item) => {
+        return (
+          <Link
+            key={nanoid()}
+            to={item.path}
+            style={{
+              color: item.title === currentPage ? "rgb(59, 130, 246)" : "#fff",
+            }}
+          >
+            <li onClick={() => func(item.title)}>{item.title}</li>
+          </Link>
+        );
+      })}
+    </ul>
   );
 };
 
